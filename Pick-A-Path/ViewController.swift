80060bff8e11303d5281302ba6b81154dbe852d5
Katelyn Patrick <Patr0142@algonquinlive.com>
2020-11-19 13:47:38 -0500

    Initial Commit

---
+++ Pick-A-Path/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pick-A-Path
+//
+//  Created by Katelyn Patrick on 2020-11-19.
+//  Copyright © 2020 Katelyn Patrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pick-A-PathTests/Pick_A_PathTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pick_A_PathTests.swift
+//  Pick-A-PathTests
+//
+//  Created by Katelyn Patrick on 2020-11-19.
+//  Copyright © 2020 Katelyn Patrick. All rights reserved.
+//
+
+import XCTest
+@testable import Pick_A_Path
+
+class Pick_A_PathTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
